353ebebc7bf57dfff482f29301e705c33ba1cb79
Oliver Wegner <void1976@gmail.com>
2013-11-06 23:50:58 +0100

    added compile-stream/ow function

--- src/compiler.lisp
+++ src/compiler.lisp
@@ -1,3 +1,14 @@
 (in-package :owlisp)
 
-(export '())
+(export '(compile-stream/ow))
+
+(defun compile-stream/ow (stream)
+  (let ((sexpr-list '()))
+    (handler-case
+	(do ((sexpr (read stream) (read stream)))
+	    (nil)
+	  (setf sexpr-list
+		(append sexpr-list (list sexpr))))
+      (end-of-file (e)
+	(declare (ignore e))
+	sexpr-list))))
