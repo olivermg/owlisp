b4b5e856a526429d753b5548fb944c4d2a66bdbb
Oliver Wegner <void1976@gmail.com>
2014-03-25 22:37:27 +0100

    implemented analyzing phase (according to SICP)

--- src/evaluator/evaluator.lisp
+++ src/evaluator/evaluator.lisp
@@ -11,6 +11,8 @@
   (string-equal (symbol-name symbol)
 		(symbol-name name)))
 
+
+
 (defun evaluate-stdin (env)
   (evaluate-stream *standard-input* env))
 
@@ -36,6 +38,7 @@
 (defun evaluate-forms (forms env)
   (evaluate-forms-to-last forms env))
 
+#|
 (defun evaluate-form (expr env)
   (format t "~%EVALUATE-FORM: ~A~%" expr)
   (cond ((self-evaluating-p expr) expr)
@@ -45,12 +48,47 @@
 					 (lambda-body expr)
 					 env))
 	((let-p expr) (evaluate-forms (let-body expr)
-				      (let-environment expr env)))
+				      (let-environment (let-bindings expr) env)))
 	((application-p expr) (apply (evaluate-form (operator expr) env)
 				     (evaluate-forms-to-list (operands expr) env)))))
+|#
+
+(defun evaluate-form (expr env)
+  (funcall (analyze expr)
+	   env))
+
+
+
+(defun analyze (expr)
+  (format t "~%ANALYZE: ~A~%" expr)
+  (cond ((self-evaluating-p expr) (analyze-self-evaluating expr))
+	((quote-p expr) (analyze-quote expr))
+	((variable-p expr) (analyze-variable expr))
+	((lambda-p expr) (analyze-lambda expr))
+	((let-p expr) (analyze-let expr))
+	((application-p expr) (analyze-application expr))))
+
+(defun analyze-sequence (exprs)
+  (let ((procs (mapcar #'analyze exprs)))
+    (lambda (env)
+      (let ((last-val nil))
+	(loop
+	   for proc in procs
+	   do (setf last-val
+		    (funcall proc env)))
+	last-val))))
+
+(defun analyze-bindings (bindings)
+  (mapcar (lambda (binding)
+	    (let ((var (first binding))
+		  (value (second binding)))
+	      (list var (analyze value))))
+	  bindings))
+
+
 
 (defun apply (proc-def args)
-  (format t "APPLY ~a ~a~%" proc-def args)
+  (format t "~%APPLY ~a ~a~%" proc-def args)
   (cond ((compound-procedure-p proc-def)
 	 (evaluate-forms (compound-procedure-body proc-def)
 			 (make-environment (compound-procedure-env proc-def)
@@ -64,6 +102,21 @@
 
 (defun apply-primitive-procedure (implementation args)
   (cl:apply implementation args))
+
+
+
+(defun execute-apply (proc-def args)
+  (format t "~%EXECUTE-APPLY ~a ~a~%" proc-def args)
+  (cond ((compound-procedure-p proc-def)
+	 (funcall (compound-procedure-body proc-def)
+		  (make-environment (compound-procedure-env proc-def)
+				    (compound-procedure-params proc-def)
+				    args)))
+	((primitive-procedure-p proc-def)
+	 (apply-primitive-procedure (primitive-procedure-implementation proc-def)
+				    args))
+	(t (error 'unknown-form
+		  :name proc-def))))
 
 
 
@@ -135,11 +188,52 @@
 (defun primitive-procedure-implementation (proc-definition)
   (second proc-definition))
 
-(defun let-environment (expr base-env)
-  (let ((bindings (second expr)))
-    (make-environment base-env
-		      (mapcar #'car bindings)
-		      (mapcar #'cadr bindings))))
+(defun let-environment (bindings base-env)
+  (make-environment base-env
+		    (mapcar #'car bindings)
+		    (mapcar (lambda (binding-proc)
+			      (funcall binding-proc base-env))
+			    (mapcar #'cadr bindings))))
+
+(defun let-bindings (expr)
+  (second expr))
 
 (defun let-body (expr)
   (rest (rest expr)))
+
+
+
+(defun analyze-self-evaluating (expr)
+  (lambda (env)
+    (declare (ignore env))
+    expr))
+
+(defun analyze-quote (expr)
+  (lambda (env)
+    (declare (ignore env))
+    (quoted-text expr)))
+
+(defun analyze-variable (expr)
+  (lambda (env)
+    (lookup-variable-value expr env)))
+
+(defun analyze-lambda (expr)
+  (let ((params (lambda-parameters expr))
+	(bodyproc (analyze-sequence (lambda-body expr))))
+    (lambda (env)
+      (make-procedure params bodyproc env))))
+
+(defun analyze-let (expr)
+  (let ((bodyproc (analyze-sequence (let-body expr)))
+	(bindings (analyze-bindings (let-bindings expr))))
+    (lambda (env)
+      (funcall bodyproc (let-environment bindings env)))))
+
+(defun analyze-application (expr)
+  (let ((operator-proc (analyze (operator expr)))
+	(operands-procs (mapcar #'analyze (operands expr))))
+    (lambda (env)
+      (execute-apply (funcall operator-proc env)
+		     (mapcar (lambda (operand-proc)
+			       (funcall operand-proc env))
+			     operands-procs)))))
