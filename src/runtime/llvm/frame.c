e36da801936af6ec6eb93ccc20d61709c05cbce3
Oliver Wegner <void1976@gmail.com>
2014-05-21 23:03:49 +0200

    implementing frames in llvm runtime

--- src/runtime/llvm/frame.c
+++ src/runtime/llvm/frame.c
@@ -4,21 +4,61 @@
 
 struct frame* new_frame( struct frame* parent )
 {
-	struct frame* newframe = calloc( 1, sizeof( struct frame ) );
-	newframe->parent = parent;
+  struct frame* newframe = calloc( 1, sizeof( struct frame ) );
+  newframe->parent = parent;
 
-	return newframe;
+  return newframe;
 }
 
-struct frame* delete_frame( struct frame* theframe )
+struct frame* delete_frame( struct frame* f )
 {
-	struct frame* parent = NULL;
+  struct frame* parent = NULL;
 
-	if ( theframe ) {
-		parent = theframe->parent;
-		free( theframe );
-	}
+  if ( f ) {
+    parent = f->parent;
+    free( f );
+  }
 
-	return parent;
+  return parent;
 }
 
+static struct frame* find_frame( struct frame* f, int frameindex )
+{
+  struct frame* found = f;
+
+  for ( int i = 0; i < frameindex; i++ ) {
+    if ( NULL != found ) {
+      found = found->parent;
+    } else {
+      // TODO: throw some error, since requested frame does not exist
+    }
+  }
+
+  return found;
+}
+
+void set_binding( struct frame* f, int frameindex, int varindex, int value )
+{
+  struct frame* correctframe = find_frame( f, frameindex );
+
+  if ( varindex >= 0 && varindex < sizeof( correctframe->values ) ) {
+    correctframe->values[varindex] = value;
+  } else {
+    // TODO: invalid address - throw some error in this case
+  }
+}
+
+int get_binding( struct frame* f, int frameindex, int varindex )
+{
+  int value = 0;
+
+  struct frame* correctframe = find_frame( f, frameindex );
+
+  if ( varindex >= 0 && varindex < sizeof( correctframe->values ) ) {
+    value = correctframe->values[varindex];
+  } else {
+    // TODO: throw error
+  }
+
+  return value;
+}
--- src/runtime/llvm/frame.h
+++ src/runtime/llvm/frame.h
@@ -1,5 +1,14 @@
+#ifndef __FRAME_H
+#define __FRAME_H 1
+
 struct frame {
-	struct frame* parent;
-	void* value[16];
+  struct frame* parent;
+  int values[16];
 };
 
+struct frame* new_frame( struct frame* parent );
+struct frame* delete_frame( struct frame* f );
+void set_binding( struct frame* f, int frameindex, int varindex, int value );
+int get_binding( struct frame* f, int frameindex, int varindex );
+
+#endif
