a35730ecaffe1c984ff91a36e7bc97079e4bcef3
Oliver Wegner <void1976@gmail.com>
2014-05-26 19:36:08 +0200

    some refactoring & improvement of llvm expression generating macro

--- src/compiler/llvm-ir/llvm-ir-adapter.lisp
+++ src/compiler/llvm-ir/llvm-ir-adapter.lisp
@@ -4,6 +4,25 @@
 
 
 
+(defun symbol-starts-with (symbol prefix)
+  (string-equal (subseq (symbol-name symbol)
+			0
+			(length prefix))
+		prefix))
+
+(defun unprefixed-symbol (symbol prefix)
+  (intern
+   (string-upcase
+    (subseq
+     (symbol-name symbol)
+     (length prefix)))))
+
+(defun upcase-expr (expr)
+  (cond ((consp expr) (mapcar #'upcase-expr
+			      expr))
+	((symbolp expr) (intern (string-upcase (symbol-name expr))))
+	(t expr)))
+
 (defun read-llvm-expr (stream char)
   (declare (ignore char))
   (let ((prev-case (readtable-case *readtable*)))
@@ -11,10 +30,13 @@
     (let* ((expr (read-delimited-list #\] stream))
 	   (result (loop
 		      for e in expr
-		      collect (if (string-equal (subseq (symbol-name e) 0 1) ".")
-				  (cons "~A"
-					(intern (string-upcase (subseq (symbol-name e) 1))))
-				  (cons (symbol-name e) nil)))))
+		      collect (cond
+				((consp e)
+				 (cons "~A" (upcase-expr e)))
+				((symbol-starts-with e ".")
+				 (cons "~A" (unprefixed-symbol e ".")))
+				(t
+				 (cons (symbol-name e) nil))))))
       (setf (readtable-case *readtable*) prev-case)
       `(format nil
 	       ,(format nil "~{~a~^ ~}"
