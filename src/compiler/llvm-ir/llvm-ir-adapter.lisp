4c510c4894c7effbb4f26e581c7bbff0f5152eb3
Oliver Wegner <void1976@gmail.com>
2014-06-01 11:49:22 +0200

    started implementing llvm-ir code generator

--- src/compiler/llvm-ir/llvm-ir-adapter.lisp
+++ src/compiler/llvm-ir/llvm-ir-adapter.lisp
@@ -4,12 +4,42 @@
 
 
 
+(defun make-llvm-code-generator ()
+  (let ((target-code '())
+	(register-index 0))
+
+    (macrolet ((emit (format-str &rest values)
+		 `(setf target-code
+			(append target-code
+				(list (format nil
+					      ,(concatenate 'string format-str "~%")
+					      ,@values))))))
+
+     (labels ((register-assign (type value)
+		(emit "  %~a = ~a ~a" (incf register-index) type value)
+		register-index)
+
+	      (constant (value)
+		))
+
+       (lambda (action &rest args)
+	 (case action
+	   ((:constant (apply #'constant args)))))))))
+
+
 ;; native types
+
 (defun i32 ()
   "i32")
 
 (defun i64 ()
   "i64")
+
+
+;; registers
+
+(defun constant (value)
+  )
 
 
 ;; functions
