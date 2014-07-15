e2ca2774d64179d708318ae94d23b8aa7ed3864b
Oliver Wegner <void1976@gmail.com>
2014-07-16 01:03:04 +0200

    started implementing types in runtime

--- src/llvm/crt/types.c
+++ src/llvm/crt/types.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "error.h"
 #include "types.h"
 
-value_t* new_value_int( int val )
+value_t* new_value( type_t type, union value_container_t val )
 {
   value_t* new_val = calloc( 1, sizeof( value_t ) );
-  new_val->type = TYPE_INT;
+  new_val->type = type;
   new_val->value = val;
 
   return new_val;
@@ -22,7 +23,46 @@ unsigned char values_equal( const value_t* value1, const value_t* value2 )
   unsigned char equal = 0;
 
   if ( value1->type == value2->type ) { // TODO: for now, do only strong typing
-    equal = value1->value == value2->value;
+    switch ( value1->type ) {
+    case TYPE_NUMBER:
+      equal = value1->value.number == value2->value.number;
+      break;
+    case TYPE_CHARACTER:
+      break;
+    case TYPE_SYMBOL:
+      break;
+    case TYPE_LIST:
+      break;
+    case TYPE_ARRAY:
+      break;
+    case TYPE_HASHTABLE:
+      break;
+    case TYPE_READTABLE:
+      break;
+    case TYPE_PACKAGE:
+      break;
+    case TYPE_PATHNAME:
+      break;
+    case TYPE_STREAM:
+      break;
+    case TYPE_RANDOMSTATE:
+      break;
+    case TYPE_STRUCTURE:
+      break;
+    case TYPE_FUNCTION:
+      break;
+    case TYPE_CONDITION:
+      break;
+    case TYPE_CLASS:
+      break;
+    case TYPE_METHOD:
+      break;
+    case TYPE_GENERICFUNCTION:
+      break;
+    default:
+      fatal_error( "cannot compare values (unknown type)!" );
+      break;
+    }
   }
 
   return equal;
@@ -30,7 +70,7 @@ unsigned char values_equal( const value_t* value1, const value_t* value2 )
 
 unsigned char is_value_true( const value_t* value )
 {
-  if ( value->value ) {
+  if ( value->value.number ) {
     return 1;
   } else {
     return 0;
--- src/llvm/crt/types.h
+++ src/llvm/crt/types.h
@@ -3,19 +3,50 @@
 
 typedef unsigned char type_t;
 
-#define TYPE_INT      0x01
-#define TYPE_FLOAT    0x02
-#define TYPE_STRING   0x03
-#define TYPE_CONS     0x04
-#define TYPE_PATH     0x05
-#define TYPE_ARRAY    0x06
+#define TYPE_NUMBER            0x01
+#define TYPE_CHARACTER         0x02
+#define TYPE_SYMBOL            0x03
+#define TYPE_LIST              0x04
+#define TYPE_ARRAY             0x05
+#define TYPE_HASHTABLE         0x06
+#define TYPE_READTABLE         0x07
+#define TYPE_PACKAGE           0x08
+#define TYPE_PATHNAME          0x09
+#define TYPE_STREAM            0x10
+#define TYPE_RANDOMSTATE       0x11
+#define TYPE_STRUCTURE         0x12
+#define TYPE_FUNCTION          0x13
+#define TYPE_CONDITION         0x14
+#define TYPE_CLASS             0x15
+#define TYPE_METHOD            0x16
+#define TYPE_GENERICFUNCTION   0x17
+
+union value_container_t {
+  long number;
+  unsigned char* character;
+  unsigned char* symbol;
+  void* list;
+  void* array;
+  void* hashtable;
+  void* readtable;
+  unsigned char* package;
+  unsigned char* pathname;
+  long stream;
+  long randomstate;
+  void* structure;
+  void* function;
+  void* condition;
+  void* class;
+  void* method;
+  void* genericfunction;
+};
 
 typedef struct _value_t {
   type_t type;
-  int value;
+  union value_container_t value;
 } value_t;
 
-value_t* new_value_int( int val );
+value_t* new_value( type_t type, union value_container_t val );
 void free_value( value_t* value );
 unsigned char values_equal( const value_t* value1, const value_t* value2 );
 unsigned char is_value_true( const value_t* value );
