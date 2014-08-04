2725aed4d63f441b0d40e9333d85d125b9099705
Oliver Wegner <void1976@gmail.com>
2014-08-04 21:37:20 +0200

    working on cps transformation

--- src/analyzer/cps.lisp
+++ src/analyzer/cps.lisp
@@ -7,64 +7,64 @@
   '(funcall apply + * - / car cdr mapcar print format))
 
 
-(defmacro cps-transform (expr k)
+(defmacro cps-transform (expr)
   (cond ((self-evaluating-p expr)
-	 `(cps-transform-self-evaluating ,expr ,k))
+	 `(cps-transform-self-evaluating ,expr))
 
 	((quote-p expr)
-	 `(cps-transform-quote ,expr ,k))
+	 `(cps-transform-quote ,expr))
 
 	((variable-p expr)
-	 `(cps-transform-variable ,expr ,k))
+	 `(cps-transform-variable ,expr))
 
 	((lambda-p expr)
-	 `(cps-transform-lambda ,expr ,k))
+	 `(cps-transform-lambda ,expr))
 
 	((application-p expr)
-	 `(cps-transform-application ,expr ,k))
+	 `(cps-transform-application ,expr))
 
 	(t (error "don't know how to cps transform ~a" expr))))
 
-(defmacro cps-transform-self-evaluating (expr k)
+(defmacro cps-transform-self-evaluating (expr)
   `(lambda (k)
      (funcall k ,expr)))
 
-(defmacro cps-transform-quote (expr k)
+(defmacro cps-transform-quote (expr)
   `(lambda (k)
      (funcall k ,expr)))
 
-(defmacro cps-transform-variable (expr k)
+(defmacro cps-transform-variable (expr)
   `(lambda (k)
      (funcall k ,expr)))
 
-(defmacro cps-transform-lambda (expr k)
+(defmacro cps-transform-lambda (expr)
   (let* ((lam (first expr))
 	 (arglist (second expr))
 	 (body (rest (rest expr)))
 	 (dyn-k (gensym)) ; TODO: this is not 100% collision safe as argument name
 	 (arglist-k (cons dyn-k arglist)))
-    `(,lam ,arglist-k
-	   (invoke/k ,k ,dyn-k
-		     (cps-transform-sequence ,body ,dyn-k)))))
+    `(lambda (k)
+       (funcall k
+		(,lam ,arglist-k
+		      (funcall ,dyn-k
+			       (cps-transform-sequence ,body)))))))
 
-(defmacro cps-transform-application (expr k)
+(defmacro cps-transform-application (expr)
   (let* ((fn (first expr))
-	 (paramlist (rest expr))
-	 (paramlist-k (cons k paramlist)))
+	 (paramlist (rest expr)))
     ;`((cps-transform ,fn ,k) ,@paramlist-k)
-    `(,fn (cps-transform-sequence ,paramlist-k ,k))))
+    `(lambda (k)
+       (,fn (cps-transform-sequence (cons k ,paramlist))))))
 
-(defmacro cps-transform-sequence (exprs k)
+(defmacro cps-transform-sequence (exprs)
   (if (consp exprs)
       (let* ((e (first exprs))
 	     (e* (rest exprs))
 	     (cur-k (if (consp e*)
 			`(lambda (ret)
 			   (declare (ignore ret))
-			   (cps-transform-sequence ,e* ,k))
+			   (cps-transform-sequence ,e*))
 			`(lambda (ret)
-			   (funcall ,k ret)))))
-	`(cps-transform ,e ,cur-k))))
-
-(defun invoke/k (k fn &rest args)
-  (funcall k (cl:apply fn args)))
+			   (funcall ret)))))
+	;`(cps-transform ,e)
+	cur-k)))
