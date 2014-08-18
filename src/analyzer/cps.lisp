d5911c5df7a153dfff4db4ad9e857e768c93de46
Oliver Wegner <void1976@gmail.com>
2014-08-18 22:49:15 +0200

    first version of cps transform

--- src/analyzer/cps.lisp
+++ src/analyzer/cps.lisp
@@ -8,7 +8,7 @@
 
 
 (defmacro cps-transform (expr)
-  (format t "cps-transform processing on ~a~%" expr)
+  (format t "~%cps-transform processing on ~a~%" expr)
   (cond ((self-evaluating-p expr)
 	 `(cps-transform-self-evaluating ,expr))
 
@@ -34,61 +34,86 @@
 
 (defmacro cps-transform-self-evaluating (expr)
   (format t "cps-transform-self-evaluating ~a~%" expr)
-  `(lambda (k)
-     (funcall k ,expr)))
+  (let ((result `(lambda (k)
+		   (funcall k ,expr))))
+    (format t "  result: ~a~%" result)
+    result))
 
 (defmacro cps-transform-quote (expr)
   (format t "cps-transform-quote ~a~%" expr)
-  `(lambda (k)
-     (funcall k ,expr)))
+  (let ((result `(lambda (k)
+		   (funcall k ,expr))))
+    (format t "  result: ~a~%" result)
+    result))
 
 (defmacro cps-transform-variable (expr)
   (format t "cps-transform-variable ~a~%" expr)
-  `(lambda (k)
-     (funcall k ,expr)))
+  (let ((result `(lambda (k)
+		   (funcall k ,expr))))
+    (format t "  result: ~a~%" result)
+    result))
 
 (defmacro cps-transform-lambda ((lam (&rest arglist) &rest body))
   (format t "cps-transform-lambda lam:~a arglist:~a body:~a~%" lam arglist body)
-  (let* ((dyn-k (gensym)) ; TODO: this is not 100% collision safe as argument name
-	 (arglist-k (cons dyn-k arglist)))
-    `(lambda (k)
-       (funcall k
-		(,lam ,arglist-k
-		      (funcall (cps-transform-sequence ,@body)
-			       ,dyn-k))))))
+  (let ((result (let* ((dyn-k (gensym)) ; TODO: this is not 100% collision safe as argument name
+		       (arglist-k (cons dyn-k arglist)))
+		  `(lambda (k)
+		     (funcall k
+			      (,lam ,arglist-k
+				    (funcall (cps-transform-sequence ,@body)
+					     ,dyn-k)))))))
+    (format t "  result: ~a~%" result)
+    result))
 
-(defmacro cps-transform-application ((fn param))
-  ;; TODO: this only implements applications with exactly one parameter. in order for
-  ;;       this to be sufficient, we need to transform applications to currying form
-  ;;       before.
-  ;;       we also need to take care of applications with no parameters at all.
-  (format t "cps-transform-application fn:~a param:~a~%" fn param)
-  (if (primitive-p fn)
-      `(lambda (k)
-	 (cl:apply k (,fn ,param)))
-      `(lambda (k)
-	 (funcall (cps-transform ,fn)
-		  (lambda (fn-result)
-		    (funcall (cps-transform ,param)
-			     (lambda (param-result)
-			       (funcall fn-result k param-result))))))))
+(defmacro cps-transform-application ((fn &rest params))
+  (format t "cps-transform-application fn:~a params:~a~%" fn params)
+  (let ((result (if (primitive-p fn)
+		    `(lambda (k)
+		       (funcall k (,fn ,@params)))
+		    (labels ((tp (fn k paramsv &rest params)
+			       (if (consp params)
+				   (let ((newparamv (gensym)))
+				     `(funcall (cps-transform ,(car params))
+					       (lambda (,newparamv)
+						 ,(cl:apply #'tp
+							    fn
+							    k
+							    (append paramsv (list newparamv))
+							    (cdr params)))))
+				   `(funcall ,fn
+					     ,k
+					     ,@paramsv))))
+		      `(lambda (k)
+			 (funcall (cps-transform ,fn)
+				  (lambda (fn-result)
+				    ,(cl:apply #'tp
+					       'fn-result
+					       'k
+					       '()
+					       params))))))))
+    (format t "  result: ~a~%" result)
+    result))
 
 (defmacro cps-transform-params (head . rest)
   (format t "cps-transform-params head:~a rest:~a~%" head rest)
-  (if (consp rest)
-      `(lambda (k)
-	 (funcall (cps-transform ,head)
-		  (lambda ())))))
+  (let ((result (if (consp rest)
+		    `(lambda (k)
+		       (funcall (cps-transform ,head)
+				(lambda ()))))))
+    (format t "  result: ~a~%" result)
+    result))
 
 (defmacro cps-transform-sequence (head . rest)
   (format t "cps-transform-sequence head:~a rest:~a~%" head rest)
-  (if (consp rest)
-      `(lambda (k)
-	 (funcall (cps-transform ,head)
-		  (lambda (discard-k)
-		    (declare (ignore discard-k))
-		    (funcall (cps-transform-sequence ,@rest)
-			     k))))
-      `(lambda (k)
-	 (funcall (cps-transform ,head)
-		  k))))
+  (let ((result (if (consp rest)
+		    `(lambda (k)
+		       (funcall (cps-transform ,head)
+				(lambda (discard-k)
+				  (declare (ignore discard-k))
+				  (funcall (cps-transform-sequence ,@rest)
+					   k))))
+		    `(lambda (k)
+		       (funcall (cps-transform ,head)
+				k)))))
+    (format t "  result: ~a~%" result)
+    result))
