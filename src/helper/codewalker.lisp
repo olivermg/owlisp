97a127814f9a2da50188cd0a07af7dd9ad808a18
Oliver Wegner <void1976@gmail.com>
2014-08-19 18:03:27 +0200

    handle symbol names in codewalker

--- src/helper/codewalker.lisp
+++ src/helper/codewalker.lisp
@@ -7,10 +7,15 @@
   (make-hash-table))
 
 
+(defun generalize-symbol (symbol)
+  (intern (symbol-name symbol)
+	  'keyword))
+
 (defmacro walk (expr)
   (if (consp expr)
       (let* ((form-name (car expr))
-	     (form-hook (gethash form-name *walker-hooks*)))
+	     (form-name-key (generalize-symbol form-name))
+	     (form-hook (gethash form-name-key *walker-hooks*)))
 	(if form-hook
 	    (funcall form-hook expr)
 	    expr))
@@ -18,7 +23,8 @@
 
 (defmacro defwalker-transformation ((form-name &rest pattern) &body transformation)
   (let ((form-arg (gensym)))
-    `(setf (gethash ',form-name *walker-hooks*)
+    `(setf (gethash (generalize-symbol ',form-name)
+		    *walker-hooks*)
 	   #'(lambda (,form-arg)
 	       (destructuring-bind ,pattern
 		   (cdr ,form-arg)
@@ -26,4 +32,4 @@
 
 (defwalker-transformation
     (fn a b c)
-    (+ c b a))
+  `(+ ,c ,b ,a))
