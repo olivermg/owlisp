dce2901e2bbf0a3fe620d3e7f7a01be3fc6a1d28
Oliver Wegner <void1976@gmail.com>
2014-09-05 03:39:10 +0200

    working on cdumper

--- owlisp-ng/cdumper.lisp
+++ owlisp-ng/cdumper.lisp
@@ -1,8 +1,50 @@
 (in-package #:owlisp)
 
 
-(defparameter *cdumper-stream* *standard-output*)
+(defmacro with-dumper (&body body)
+  `(labels ((new-buf ()
+	      (make-array '(0) :element-type 'base-char :fill-pointer 0 :adjustable t)))
+     (let* ((*varname-index* 0)
+	    (*procedurename-index* 0)
+	    (*dumper-buffers* (list (new-buf)))
+	    (*current-buffer* (car *dumper-buffers*)))
+       (declare (special *varname-index* *procedurename-index* *dumper-buffers* *current-buffer*))
+       ,@body
+       (apply #'concatenate
+	      'string
+	      *dumper-buffers*))))
 
 
-(defun dump (text)
-  (format *cdumper-stream* text))
+(defun dump (formatstr &rest args)
+  (declare (special *dumper-buffer*))
+  (setf *dumper-buffer*
+	(concatenate 'string
+		     *dumper-buffer*
+		     (apply #'format nil formatstr args))))
+
+(defun dump-global (formatstr &rest args)
+  (declare (special *dumper-buffer*))
+  (setf *dumper-buffer*
+	(concatenate 'string
+		     (apply #'format nil formatstr args)
+		     *dumper-buffer*)))
+
+
+(defun ow.next-varname ()
+  (intern
+   (format nil "VAR~d" (incf *varname-index*))))
+
+(defun ow.next-procedurename ()
+  (intern
+   (format nil "PROC~d" (incf *procedurename-index*))))
+
+
+(defun dump-assignment (value)
+  (let ((varname (ow.next-varname)))
+    (dump "int ~a = ~a;~%" varname value)
+    varname))
+
+(defun dump-fndefinition (body)
+  (let ((procname (ow.next-procedurename)))
+    (dump-global "int ~a() {~%}~%" procname)
+    procname))
--- owlisp-ng/compiler.lisp
+++ owlisp-ng/compiler.lisp
@@ -4,8 +4,6 @@
 
 
 (defparameter *global-env* (env.extend '()))
-(defparameter *varname-index* 0)
-(defparameter *procedurename-index* 0)
 (defparameter *symbol-table* (make-hash-table))
 
 
@@ -16,57 +14,53 @@
 
     ((atom expr) (ow.compile-atom expr env))
 
-    (t (case (intern (symbol-name (car expr)) 'keyword)
+    (t (if (atom (car expr))
 
-	 (:QUOTE (ow.compile-quoted expr env))
+	   (case (intern (symbol-name (car expr)) 'keyword)
 
-	 (:LAMBDA (ow.compile-abstraction (cadr expr) (cddr expr) env))
+	     (:QUOTE (ow.compile-quoted expr env))
 
-	 (t (ow.compile-application (car expr) (cdr expr) env))))))
+	     (:LAMBDA (ow.compile-abstraction (cadr expr) (cddr expr) env))
 
-(defun ow.compile-sequence (exprs &optional (compiled "") (env *global-env*))
+	     (t (ow.compile-application (car expr) (cdr expr) env)))
+
+	   (ow.compile-application (car expr) (cdr expr) env)))))
+
+(defun ow.compile-sequence (exprs &optional (env *global-env*) compiled)
   (if exprs
       (ow.compile-sequence (cdr exprs)
-			   (concatenate 'string
-					compiled
-					(ow.compile (car exprs) env))
-			   env)
-      (format nil "{~%~a}~%" compiled)))
+			   env
+			   (append compiled
+				   (list (ow.compile (car exprs) env))))
+      compiled))
 
 
 (defun ow.compile-reference (symbol &optional (env *global-env*))
   (let ((address (funcall env symbol)))
-    (format nil "lookup( ~a, ~a );~%" (car address) (cdr address))))
+    (format nil "lookup( ~a, ~a )" (car address) (cdr address))))
 
 (defun ow.compile-atom (value &optional (env *global-env*))
-  (format nil "~a;~%" value))
+  (declare (ignore env))
+  (format nil "~a" value))
 
 (defun ow.compile-quoted (symbol &optional (env *global-env*))
+  (declare (ignore env))
   (format nil "quoted: ~a~%" symbol))
 
 (defun ow.compile-abstraction (args body &optional (env *global-env*))
-  (let ((resultvar (ow.next-varname))
-	(procname (ow.next-procedurename))
-	(ext-env (env.extend args env)))
+  (let* ((resultvar (ow.next-varname))
+	 (procname (ow.next-procedurename))
+	 (ext-env (env.extend args env))
+	 (compiled-body (format nil "{~%~{  ~a;~%~}}~%" (ow.compile-sequence body ext-env))))
     (setf (gethash procname *symbol-table*)
-	  (list args ext-env))
+	  (list procname args ext-env))
     (concatenate 'string
 		 (format nil "int ~a() {~%" procname)
-		 (format nil "int ~a = ~a" resultvar (ow.compile-sequence body "" ext-env))
+		 (format nil "int ~a = ~a~%" resultvar compiled-body)
 		 (format nil "return ~a;~%" resultvar)
 		 (format nil "}~%"))))
 
 (defun ow.compile-application (operator params &optional (env *global-env*))
-  (let ((procname (ow.next-procedurename)))
-    (concatenate 'string
-		 (format nil "void* (*~a)() = lookup_procedure( ~a );~%" procname operator)
-		 (format nil "invoke( ~a, ~a );~%" procname params))))
-
-
-(defun ow.next-varname ()
-  (intern
-   (format nil "var~4,'0d" (incf *varname-index*))))
-
-(defun ow.next-procedurename ()
-  (intern
-   (format nil "proc~4,'0d" (incf *procedurename-index*))))
+  (let ((compiled-operator (ow.compile operator env))
+	(compiled-params (ow.compile-sequence params env)))
+   (format nil "invoke( ~a, ~{~a~^, ~} );~%" compiled-operator compiled-params)))
