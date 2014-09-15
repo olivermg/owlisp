fdcf07158cd40095ee91eb5e151fc1066715d786
Oliver Wegner <void1976@gmail.com>
2014-09-15 21:07:16 +0200

    started writing macros for closure conversion

--- src/analyzer/closure.lisp
+++ src/analyzer/closure.lisp
@@ -3,3 +3,27 @@
 (export '())
 
 
+(defparameter *closure-conversion-definitions* '())
+
+
+(defwalker-rule *closure-conversion-definitions*
+
+    #'(lambda (expr)
+	(variable-p expr))
+
+  (expr nil)
+
+  )
+
+
+(defmacro call-closure (&rest args)
+  `(funcall (car (get-closure)) ,@args))
+
+(defmacro closure-lambda ((&rest args) &body body)
+  (let ((closure-var (gensym)))
+    `(macrolet ((get-closure ()
+		  ,closure-var)
+		(lookup-symbol (symbol)
+		  ))
+       (lambda (,closure-var ,@args)
+	 ,@body))))
