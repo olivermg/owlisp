f4f00af37fdef73b2c2090f9662a5fc08717d2d6
Oliver Wegner <void1976@gmail.com>
2014-10-06 23:29:55 +0200

    implementing walker to dump to c

--- src/ctarget/walker.lisp
+++ src/ctarget/walker.lisp
@@ -13,4 +13,29 @@
 
 	(obj nil)
 
-      (dump-constant (constant*-value obj)))))
+      (dump-constant (constant*-value obj)))
+
+    (defrule
+
+	#'symbol*-p
+
+	(obj nil)
+
+      (error "not implemented yet"))
+
+    (defrule
+
+	#'reference*-p
+
+	(obj nil)
+
+      (dump-reference (reference*-symbol obj) ; TODO: change this to frame- & varindex
+		      (reference*-symbol obj)))
+
+    (defrule
+
+	#'abstraction*-p
+
+	(obj nil)
+
+      (dump-fndefinition-start ()))))
