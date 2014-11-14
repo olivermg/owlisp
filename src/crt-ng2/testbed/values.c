ac75d67667d62375b260c3f3f22e4d6697e264ec
Oliver Wegner <void1976@gmail.com>
2014-11-15 00:47:56 +0100

    implement (un)boxing in test c file

--- src/crt-ng2/testbed/values.c
+++ src/crt-ng2/testbed/values.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
-#define value(t,v) \
-	{ t, { .F_##t = v } }
+#define box_value(t,v) \
+  { t, { .t = v } }
+
+#define unbox_value(t,vx) \
+  vx.v.t
 
 
-#define F_INT i
-#define F_PROC p
 typedef enum _type_t {
-	UNDEF = 0,
-	INT,
-	PROC,
+  UNDEF = 0,
+  INT,
+  PROC,
 } type_t;
 
 struct _value_t;
 
 typedef union _val {
-	int i;
-	struct _value_t (*p)(struct _value_t*);
+  int INT;
+  struct _value_t (*PROC)(struct _value_t*);
 } val;
 
 typedef struct _value_t {
-	type_t t;
-	val v;
+  type_t t;
+  val v;
 } value_t;
 
 typedef value_t value_t_list[8];
@@ -29,25 +30,27 @@ typedef value_t value_t_list[8];
 
 value_t proc1(value_t_list vs)
 {
-	printf("called proc1 with %d and %d\n", vs[0].v.i, vs[1].v.i);
+  int int1 = unbox_value(INT, vs[0]);
+  int int2 = unbox_value(INT, vs[1]);
 
-	value_t vr = value( INT, vs[0].v.i + vs[1].v.i );
+  printf("called proc1 with %d and %d\n", int1, int2);
 
-	return vr;	// FIXME: unsafe!
+  value_t vr = box_value( INT, int1 + int2 );
+
+  return vr;	// FIXME: unsafe!
 }
 
 int main()
 {
-	value_t v1 = value( INT, 22 );
-	value_t v2 = value( INT, 33 );
-	value_t_list vs = { v1, v2 };
+  value_t v1 = box_value( INT, 22 );
+  value_t v2 = box_value( INT, 33 );
+  value_t_list vs = { v1, v2 };
 
-	value_t vp = value( PROC, &proc1 );
+  value_t vp = box_value( PROC, &proc1 );
 
-	value_t vr = vp.v.p(vs);
+  value_t vr = unbox_value(PROC, vp)(vs);
 
-	printf("result: %d\n", vr.v.i);
+  printf("result: %d\n", unbox_value(INT, vr));
 
-	return vr.v.i;
+  return unbox_value(INT, vr);
 }
-
