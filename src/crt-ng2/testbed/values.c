a9fc6fbe1ff79f6dd31b79f138063849611d2b62
Oliver Wegner <void1976@gmail.com>
2014-11-14 20:15:22 +0100

    extending testbed for runtime

--- src/crt-ng2/testbed/values.c
+++ src/crt-ng2/testbed/values.c
@@ -1,35 +1,53 @@
 #include <stdio.h>
 
-#define ARGLISTSIZE	8
+#define value(t,v) \
+	{ t, { .F_##t = v } }
 
+
+#define F_INT i
+#define F_PROC p
+typedef enum _type_t {
+	UNDEF = 0,
+	INT,
+	PROC,
+} type_t;
+
+struct _value_t;
 
 typedef union _val {
 	int i;
-	union _val (*p)(union _val*);
+	struct _value_t (*p)(struct _value_t*);
 } val;
 
-typedef val val_list[ARGLISTSIZE];
+typedef struct _value_t {
+	type_t t;
+	val v;
+} value_t;
+
+typedef value_t value_t_list[8];
 
 
-val proc1(val_list vs)
+value_t proc1(value_t_list vs)
 {
-	printf("called proc1 with %d and %d\n", vs[0].i, vs[1].i);
+	printf("called proc1 with %d and %d\n", vs[0].v.i, vs[1].v.i);
 
-	val vr = { .i = vs[0].i + vs[1].i };
+	value_t vr = value( INT, vs[0].v.i + vs[1].v.i );
 
 	return vr;	// FIXME: unsafe!
 }
 
 int main()
 {
-	val v1 = { .i = 22 };
-	val v2 = { .i = 33 };
-	val_list vs = { v1, v2 };
+	value_t v1 = value( INT, 22 );
+	value_t v2 = value( INT, 33 );
+	value_t_list vs = { v1, v2 };
 
-	val vp = { .p = &proc1 };
+	value_t vp = value( PROC, &proc1 );
 
-	val v3 = vp.p(vs);
+	value_t vr = vp.v.p(vs);
 
-	return v3.i;
+	printf("result: %d\n", vr.v.i);
+
+	return vr.v.i;
 }
 
