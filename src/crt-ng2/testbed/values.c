23f56d45a4260aa7ec436ceed242d841cd66cee8
Oliver Wegner <void1976@gmail.com>
2014-11-19 00:00:23 +0100

    implementing add() for c test

--- src/crt-ng2/testbed/values.c
+++ src/crt-ng2/testbed/values.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdarg.h>
+
 
 #define box_value(t,v) \
   { t, { .t = v } }
@@ -26,17 +28,40 @@ typedef struct _value_t {
   val v;
 } value_t;
 
-typedef value_t value_t_list[8];
+typedef value_t value_t_list[8]; // TODO: why don't we want to use va_arg?
+
+
+value_t add(int numargs, ...)
+{
+  value_t sum = box_value(INT, 0);
+
+  va_list values;
+
+  va_start(values, numargs);
+  for (int i = 0; i < numargs; i++) {
+    value_t value = va_arg(values, value_t);
+    sum.v.INT += value.v.INT;
+  }
+  va_end(values);
+
+  return sum;
+}
 
 
 value_t proc1(value_t_list vs)
 {
-  int int1 = *(int*)unbox_value(vs[0]); // TODO: actually, we don't want to use any primitive types in "application level" code
+  /* TODO:
+   * actually, we don't want to use any primitive types in generated code, i.e. we don't compile code
+   * making use of "unbox_value".
+   * that results in the need to have primitive functions - like "add", for we can't use the C native
+   * operators on value_t
+   */
+  int int1 = *(int*)unbox_value(vs[0]);
   int int2 = *(int*)unbox_value(vs[1]);
 
   printf("called proc1 with %d and %d\n", int1, int2);
 
-  value_t vr = box_value( INT, int1 + int2 );
+  value_t vr = add(2, vs[0], vs[1]);
 
   return vr;	// FIXME: unsafe!
 }
