7f2bcdddcf980e022ba8e3a25ca33032b44ffec3
Oliver Wegner <void1976@gmail.com>
2014-11-22 17:55:25 +0100

    extending c test values2.c

--- src/crt-ng2/testbed/values2.c
+++ src/crt-ng2/testbed/values2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 
 typedef struct _Class Class;
@@ -32,7 +33,7 @@ struct _String {
   char* value;
 };
 
-typedef Object* (*proc_p)(Object*);
+typedef Object* (*proc_p)(unsigned long numargs, ...); // TODO: what is the best representation for args? va_arg, List*, env?
 struct _Proc {
   Object object;
   proc_p value;
@@ -96,6 +97,38 @@ Object* newlist(Object* value, List* next)
 }
 
 
+Object* add_obj(Object* o1, Object* o2)
+{
+  // TODO: type safety
+
+  Integer* i1 = (Integer*)o1;
+  Integer* i2 = (Integer*)o2;
+
+  return newint( i1->value + i2->value );
+}
+
+Object* invoke_obj(Object* op, unsigned long numargs, ...)
+{
+  // TODO: type safety
+
+  va_list args;
+  return ((Proc*)op)->value(numargs, args);
+}
+
+Object* car_obj(Object* list)
+{
+  // TODO: type safety
+
+  return ((List*)list)->value;
+}
+
+Object* cdr_obj(Object* list)
+{
+  // TODO: type safety
+
+  return (Object*)((List*)list)->next;
+}
+
 void print_obj(Object* o)
 {
   if ( NULL == o ) {
@@ -116,6 +149,7 @@ void print_obj(Object* o)
 }
 
 
+/*
 Object* identity(Object* o)
 {
   printf("identity called!");
@@ -130,12 +164,42 @@ int main()
   Object* o3 = newproc(&identity);
   Object* o4 = newlist(o1, NULL);
   Object* o5 = newlist(o2, (List*)o4);
+  Object* o6 = newint(44);
+  Object* o7 = add_obj(o1, o6);
+  Object* o8 = invoke_obj(o3, o2);
 
   print_obj(o1);
   print_obj(o2);
   print_obj(o3);
   //  print_obj(o4);
   print_obj(o5);
+  print_obj(o7);
+  print_obj(o8);
+
+  return 0;
+}
+*/
+
+Object* PROC1(unsigned long numargs, ...) {
+  va_list args;
+
+  //  Object* VAR1 = car_obj( cdr_obj( args ) );
+  va_start(args, numargs);
+  va_arg(args, Object*);
+  Object* VAR1 = va_arg(args, Object*);
+  va_end(args);
+
+  return VAR1;
+}
+
+int main() {
+  Object* VAR2 = newproc( &PROC1 );
+  Object* VAR3 = newint( 11 );
+  Object* VAR4 = newint( 22 );
+  //  Object* l = newlist( VAR3, (List*)newlist( VAR4, NULL ) );
+  Object* VAR5 = invoke_obj( VAR2, 2, VAR3, VAR4 );
+
+  print_obj( VAR5 );
 
   return 0;
 }
