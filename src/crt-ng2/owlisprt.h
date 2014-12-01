9726596b0570248a9db63f77e827a83b56586971
Oliver Wegner <void1976@gmail.com>
2014-12-01 20:28:04 +0100

    added owlisprt.h

---
+++ src/crt-ng2/owlisprt.h
@@ -0,0 +1,11 @@
+#ifndef __OWLISPRT_H
+#define __OWLISPRT_H
+
+#include "classes.h"
+#include "env.h"
+#include "invocation.h"
+#include "math_basic.h"
+#include "list.h"
+#include "print.h"
+
+#endif
